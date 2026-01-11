63636854520072db8be0c014b04b3e5b07ac4df6
shreya06s <06.shreyasrivastava@gmail.com>
2026-01-11 23:11:38 +0530

    sumfunction

---
+++ SumFunction.java
@@ -0,0 +1,18 @@
+package functions;
+
+import java.util.Scanner;
+
+public class SumFunction {
+	public static int sum(int a,int b) {
+		int c=a+b;
+		return c;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the values for sum");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.println(sum(a,b));
+	}
+}// end of the program
