e1b67e2c825910412906521a0de1b5c31b90ebfe
shreya06s <06.shreyasrivastava@gmail.com>
2026-05-04 23:55:28 +0530

    maximumconsecutive.java

---
+++ src/arrays/maximumconsecutive.java
@@ -0,0 +1,29 @@
+package arrays;
+import java.util.*;
+public class maximumconsecutive {
+
+	
+			  public int findMaxConsecutiveOnes(int[] nums) {
+			    int count=0;
+			    int maxim=0;
+			    for(int i=0;i<nums.length;i++){
+			        if(nums[i]==1){
+			        count++;
+			        maxim= Math.max(maxim,count);
+			        }
+			    else{
+			        count=0;
+			    }
+			    }
+			    return maxim;
+			  }
+			  public static void main(String args[]) {
+				  maximumconsecutiveones obj=new maximumconsecutiveones();
+				  int[] nums= {1,1,0,1,1,1};
+				  int result=obj.findMaxConsecutiveOnes(nums);
+				  System.out.println("max consecutives ones:"+result);
+				  
+			}
+	}
+
+}
